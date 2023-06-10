9de04238bcfab80c062d9de76bcf6dd8179a227b
Victor Pimentel <victorpimentelw@gmail.com>
2023-06-10 19:22:23 -0300

    Melhorias no códico e na interface.

---
+++ src/main/java/org/currentWeather/Weather.java
@@ -0,0 +1,153 @@
+package org.currentWeather;
+
+public class Weather {
+    private String city;
+
+    private String state;
+
+    private String country;
+
+    private String time;
+
+    private String day;
+
+    private String month;
+
+    private String year;
+
+    private String weekDay;
+
+    private int is_day;
+
+    private int weatherCode;
+
+    private int precipitation;
+
+    private double temperature;
+
+    private double windSpeed;
+
+    private double latitude;
+
+    private double longitude;
+
+    public String getWeekDay() {
+        return weekDay;
+    }
+
+    public void setWeekDay(String weekDay) {
+        this.weekDay = weekDay;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public int getIs_day() {
+        return is_day;
+    }
+
+    public void setIs_day(int is_day) {
+        this.is_day = is_day;
+    }
+
+    public int getWeatherCode() {
+        return weatherCode;
+    }
+
+    public void setWeatherCode(int weatherCode) {
+        this.weatherCode = weatherCode;
+    }
+
+    public int getPrecipitation() {
+        return precipitation;
+    }
+
+    public void setPrecipitation(int precipitation) {
+        this.precipitation = precipitation;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public double getWindSpeed() {
+        return windSpeed;
+    }
+
+    public void setWindSpeed(double windSpeed) {
+        this.windSpeed = windSpeed;
+    }
+}
---
+++ src/main/java/org/example/MainScreenController.java
@@ -0,0 +1,241 @@
+package org.example;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import org.currentWeather.Weather;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
+public class MainScreenController {
+
+    @FXML
+    private Label countryLabel;
+
+    @FXML
+    private Label stateLabel;
+
+    @FXML
+    private Label cityLabel;
+
+    @FXML
+    private Label currentTemperatureLabel;
+
+    @FXML
+    private Label windSpeedLabel;
+
+    @FXML
+    private Label timeLabel;
+
+    @FXML
+    private TextField searchTextField;
+
+    @FXML
+    private Button searchButton;
+
+    private JsonObject local;
+
+    private JsonObject weather;
+
+    private Weather nowWeather = new Weather();
+
+    public void searchBar() {
+        String cityName = searchTextField.getText();
+        if (cityName != null) {
+            getLocalization(cityName);
+        }
+        searchTextField.setText("");
+    }
+    public void getLocalization(String cityName) {
+        try {
+            URL url = new URL("https://geocoding-api.open-meteo.com/v1/search?name=" + cityName + "&count=1&language=pt&format=json");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String line;
+                StringBuilder response = new StringBuilder();
+
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                }
+                reader.close();
+
+                String jsonResponse = response.toString();
+                Gson gson = new GsonBuilder().setPrettyPrinting().create();
+                JsonParser parser = new JsonParser();
+                local = parser.parse(jsonResponse).getAsJsonObject();
+
+                localizationInformationExtract();
+
+            } else {
+                System.out.println("HTTP request failed with response code: " + responseCode);
+            }
+
+            connection.disconnect();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void localizationInformationExtract() {
+        if (local.has("results")) {
+            JsonObject result = local.getAsJsonArray("results").get(0).getAsJsonObject();
+
+            nowWeather.setCountry(result.get("country").getAsString());
+            nowWeather.setState(result.get("admin1").getAsString());
+            nowWeather.setCity(result.get("admin2").getAsString());
+            nowWeather.setLatitude(result.get("latitude").getAsDouble());
+            nowWeather.setLongitude(result.get("longitude").getAsDouble());
+
+            getWeather(nowWeather.getLatitude(), nowWeather.getLongitude());
+        }
+    }
+
+    public void getWeather(double latitude, double longitude) {
+        try {
+            URL url = new URL("https://api.open-meteo.com/v1/forecast?latitude="+ latitude +"&longitude="+ longitude +"&hourly=temperature_2m,relativehumidity_2m,apparent_temperature,precipitation_probability&daily=weathercode,temperature_2m_max,temperature_2m_min&current_weather=true&timezone=auto");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String line;
+                StringBuilder response = new StringBuilder();
+
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                }
+                reader.close();
+
+                String jsonResponse = response.toString();
+                Gson gson = new GsonBuilder().setPrettyPrinting().create();
+                JsonParser parser = new JsonParser();
+                weather = parser.parse(jsonResponse).getAsJsonObject();
+
+                weatherInformationExtract();
+            } else {
+                System.out.println("HTTP request failed with response code: " + responseCode);
+            }
+
+            connection.disconnect();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void weatherInformationExtract() {
+        if (weather.has("current_weather")) {
+            JsonObject currentWeatherObj = weather.get("current_weather").getAsJsonObject();
+
+            nowWeather.setTemperature(currentWeatherObj.get("temperature").getAsDouble());
+            nowWeather.setWindSpeed(currentWeatherObj.get("windspeed").getAsDouble());
+            // nowWeather.setDay nowWeather.setmonth nowWeather.setyear
+            nowWeather.setIs_day(currentWeatherObj.get("is_day").getAsInt());
+            nowWeather.setWeatherCode(currentWeatherObj.get("weathercode").getAsInt());
+
+            extractTime(currentWeatherObj.get("time").getAsString());
+            dayOfTheWeek(currentWeatherObj.get("time").getAsString());
+            updateLabels();
+        }
+    }
+
+    public void updateLabels() {
+        cityLabel.setText(nowWeather.getCity());
+        stateLabel.setText(nowWeather.getState());
+        countryLabel.setText(nowWeather.getCountry());
+
+        int temperaturaArredondada = (int) Math.round(nowWeather.getTemperature());
+        currentTemperatureLabel.setText(String.valueOf(temperaturaArredondada) + "°");
+
+        timeLabel.setText(nowWeather.getWeekDay() + ", " + nowWeather.getDay()
+                + " " + monthByNumber(nowWeather.getMonth()) + " " + nowWeather.getTime());
+    }
+
+    public void extractTime(String time) {
+
+        LocalDateTime dateTime = LocalDateTime.parse(time, DateTimeFormatter.ISO_DATE_TIME);
+
+
+        nowWeather.setDay(String.valueOf(dateTime.getDayOfMonth()));
+        nowWeather.setMonth(String.valueOf(dateTime.getMonthValue()));
+        nowWeather.setYear(String.valueOf(dateTime.getYear()));
+
+        LocalTime now = LocalTime.now();
+        int hour = now.getHour();
+        int minute = now.getMinute();
+
+        if (hour < 10) {
+            nowWeather.setTime("0"+hour + ":" + minute);
+        }else {
+            nowWeather.setTime(hour + ":" + minute);
+        }
+        if (minute < 10) {
+            nowWeather.setTime(hour + ":0" + minute);
+        }else {
+            nowWeather.setTime(hour + ":" + minute);
+        }
+    }
+
+    public void dayOfTheWeek(String time) {
+        LocalDateTime dateTime = LocalDateTime.parse(time, DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+
+        // Obter o dia da semana no estilo completo (por exemplo, "sábado")
+        String diaDaSemanaCompleto = dateTime.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.US);
+
+        // Obter o dia da semana no estilo abreviado (por exemplo, "sáb")
+        String diaDaSemanaAbreviado = dateTime.getDayOfWeek().getDisplayName(TextStyle.SHORT, Locale.US);
+
+        nowWeather.setWeekDay(diaDaSemanaCompleto);
+    }
+
+    public String monthByNumber(String month) {
+        switch (month) {
+            case "1":
+                return "January";
+            case "2":
+                return "February";
+            case "3":
+                return "March";
+            case "4":
+                return "April";
+            case "5":
+                return "May";
+            case "6":
+                return "June";
+            case "7":
+                return "July";
+            case "8":
+                return "Augusty";
+            case "9":
+                return "September";
+            case "10":
+                return "October";
+            case "11":
+                return "November";
+            case "12":
+                return "December";
+            default:
+                return "";
+        }
+    }
+}
