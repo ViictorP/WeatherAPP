6b00e19ccdcecef94bb45a9ee6ea064f97598a79
Victor Pimentel <victorpimentelw@gmail.com>
2023-07-10 15:45:56 -0300

    Ícone adicionado.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -15,6 +16,8 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("/fxmlFiles/mainScreen.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
+        Image icon = new Image(getClass().getResourceAsStream("/uiIcons/appIcon.png"));
+        stage.getIcons().add(icon);
         stage.setResizable(false);
         stage.show();
     }
